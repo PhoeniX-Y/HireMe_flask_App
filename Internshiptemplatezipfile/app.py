56a7f737e8dcdb7903ea4d026851dfda792cc739
Yeshwanth M <121927440+PhoeniX-Y@users.noreply.github.com>
2025-05-05 23:14:33 +0530

    Update app.py

--- Internshiptemplatezipfile/app.py
+++ Internshiptemplatezipfile/app.py
@@ -212,7 +212,7 @@ def api_report_data():
 # ++++++++++++++++++++++++++++++++++++++ END OF REPORT PAGE +++++++++++++++++++++++++++++
 
 if __name__ == '__main__':
-
+    init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
 
